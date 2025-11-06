b1543cd65c9b978aba33b68ea2c1156d7f773a0d
3lPr1mo <erickenriquechaparro@gmail.com>
2025-11-06 15:09:03 -0500

    refactor(main): add validator class to dto

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ProductHandler } from 'application/handler/product.handler';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidUnknownValues: true,
+      skipNullProperties: true
+    })
+  );
+
   const openApiConfig = new DocumentBuilder()
   .setTitle('Credit Payment API')
   .setDescription('Credit Payment API description')
