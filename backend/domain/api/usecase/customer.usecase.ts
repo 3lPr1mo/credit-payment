b82e51a391efcacdafa77dc6b3b734d6e55c12e6
3lPr1mo <erickenriquechaparro@gmail.com>
2025-11-06 16:09:14 -0500

    refactor(domain): refactor class to implements the customer service port interface

--- backend/domain/api/usecase/customer.usecase.ts
+++ backend/domain/api/usecase/customer.usecase.ts
@@ -4,11 +4,11 @@ import { CustomerPersistencePort } from "domain/spi/customer.persistence.port";
 import { CustomerAlreadyExistsException } from "domain/exception/customer.already.exists.exception";
 import { ExceptionConstant } from "domain/constant/exception.constants";
 
-export class CustomerUseCase extends CustomerServicePort {
+export class CustomerUseCase implements CustomerServicePort {
 
-    constructor(private readonly customerPersistencePort: CustomerPersistencePort){super();}
+    constructor(private readonly customerPersistencePort: CustomerPersistencePort){}
 
-    override async createCustomer(customer: Customer): Promise<void> {
+    async createCustomer(customer: Customer): Promise<void> {
         const clientExists = await this.customerPersistencePort.clientExistsWithEmailAndDni(customer.email, customer.dni);
         if(clientExists){
             throw new CustomerAlreadyExistsException(ExceptionConstant.CUSTOMER_ALREADY_EXISTS_MESSAGE);
